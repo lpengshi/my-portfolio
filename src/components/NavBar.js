cdf332ca08de74b9ebcf87d7ad00933c5ab30c26
lpengshi <laips90@hotmail.com>
2021-04-18 12:31:45 +0800

    initial navbar and footer

---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+const NavBar = () => {
+	return (
+		<>
+			<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+				<Navbar.Brand href="/">GREG LAI</Navbar.Brand>
+				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+				<Navbar.Collapse id="responsive-navbar-nav">
+					<Nav className="mr-auto">
+						<Nav.Link href="#aboutme">ABOUT ME</Nav.Link>
+						<Nav.Link href="#skills">SKILLS</Nav.Link>
+						<Nav.Link href="#projects">PROJECTS</Nav.Link>
+						<Nav.Link href="#contact">CONTACT</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+		</>
+	);
+};
+
+export default NavBar;
