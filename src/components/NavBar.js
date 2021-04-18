07ac4cfa43e4cfff65028d9e64623e7e93d4a272
lpengshi <laips90@hotmail.com>
2021-04-18 16:29:29 +0800

    Navbar align right

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,11 +4,12 @@ import Nav from 'react-bootstrap/Nav';
 const NavBar = () => {
 	return (
 		<>
-			<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+			<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
 				<Navbar.Brand href="/">GREG LAI</Navbar.Brand>
 				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
 				<Navbar.Collapse id="responsive-navbar-nav">
-					<Nav className="mr-auto">
+					<Nav className="ml-auto">
+						<Nav.Link href="#home">HOME</Nav.Link>
 						<Nav.Link href="#aboutme">ABOUT ME</Nav.Link>
 						<Nav.Link href="#skills">SKILLS</Nav.Link>
 						<Nav.Link href="#projects">PROJECTS</Nav.Link>
